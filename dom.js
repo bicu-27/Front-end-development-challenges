96a0be21d3fdbeea9df627e1ba04567c7970bc63
Lydia Kebede <netsaneta757@gmail.com>
2025-03-01 20:35:41 +0300

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,20 @@
+function changeText(){
+    document.getElementById('title').innerText = "Lydia Kebede";
+} 
+// Seleccting elements in dom manipulation
+
+/*
+let heading = document.querySelector("h1");
+let button = document.querySelector("#myButton");
+let items = document.querySelectorAll(".item");
+document.getElementById("myId")
+document.getElementsByClassName("myClass");
+document.getElementsByTagName("div");
+ */
+// Modifying elements
+let heading = document.querySelector("h1");
+heading.textContent = "New Heading";
+
+
+
+
---
+++ domone.js
@@ -0,0 +1,6 @@
+ let heading = document.getElementById("heading");
+ let button = document.getElementById("changeTextBtn");
+button.addEventListener("click",function(){
+    heading.textContent = "New Heading";
+
+});

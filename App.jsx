196340a20a8cd6cad183367d2c3ce79541739da8
Lydia Kebede <netsaneta757@gmail.com>
2025-03-30 19:14:32 +0300

    Add files via upload
     my first react project

---
+++ App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+   
+    <Text display=" what'up" />
+  <Text  display = " hello"/>
+    </>
+   
+  )
+}
+function  Text({display}){
+  return (
+    <div> 
+      <p>
+        {display}
+      </p>
+    </div>
+
+  )
+}
+
+export default App
